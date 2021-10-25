69c8e573c1449a4660a06247b2d618fa00368c8c
hq <1422127065@qq.com>
2021-10-25 15:47:15 +0800

    update redis事务操作

--- mysqlDataInsert/数据插入.go
+++ mysqlDataInsert/数据插入.go
@@ -9,11 +9,18 @@ import (
 	"web_app/dao/mysqlc/model"
 )
 
+type User struct {
+	gorm.Model
+	UserName string `gorm:"column:username"`
+	Password string `gorm:"column:password"`
+	Status   int    `gorm:"column:status"`
+}
+
 var db *gorm.DB
 
 func main() {
 	var err error
-	dsn := fmt.Sprintf("%s:%d@tcp(127.0.0.1:3306)/sqk_demo?charset=utf8mb4&parseTime=True&loc=Local",
+	dsn := fmt.Sprintf("%s:%d@tcp(127.0.0.1:3306)/spider?charset=utf8mb4&parseTime=True&loc=Local",
 		"root", 142212)
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
 		// 禁用外键约束
@@ -85,4 +92,17 @@ func QueryPost() {
 	for _, x := range posts {
 		fmt.Println(*x)
 	}
+}
+
+func InsertUser() {
+	var users []User
+	for x := 0; x < 150; x++ {
+		user := User{
+			UserName: fmt.Sprintf("用户名%s", strconv.Itoa(x)),
+			Password: fmt.Sprintf("密码%s", strconv.Itoa(x)),
+		}
+		users = append(users, user)
+	}
+	db.Create(&users)
+
 }
